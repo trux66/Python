d98c9febb1797e5ea70af98d5913156337d24203
Bruce Clawson <bruce@clawsonfamily.net>
2020-04-05 21:13:16 -0400

    completed sample GUI

--- sample_gui/db.py
+++ sample_gui/db.py
@@ -4,7 +4,7 @@ import sqlite3
 class Database:
     def __init__(self, db):
         self.conn = sqlite3.connect(db)
-        self.cur = self.conn.cursor
+        self.cur = self.conn.cursor()
         self.cur.execute(
             "CREATE TABLE IF NOT EXISTS parts (id INTEGER PRIMARY KEY, part text, customer text, retailer text, price text)")
         self.conn.commit()
@@ -32,11 +32,11 @@ class Database:
         self.conn.close()
 
 
-db = Database('store.db')
-db.insert("4GB DDR4 Ram", "John Doe", "Microcenter", "160")
-db.insert("Asus Mobo", "Mike Henry", "Microcenter", "160")
-db.insert("500w PSU", "Karen Johnson", "Newegg", "80")
-db.insert("2GB DDR4 Ram", "Karen Johnson", "Newegg", "70")
-db.insert("24 inch Samsung Monitor", "Sam Smith", "Best Buy", "180")
-db.insert("NVIDIA RTX 2080", "Albert Kingston", "Newegg", "679")
-db.insert("600w Corsair PSU", "Karen Johnson", "Newegg", "130")
+# db = Database('store.db')
+# db.insert("4GB DDR4 Ram", "John Doe", "Microcenter", "160")
+# db.insert("Asus Mobo", "Mike Henry", "Microcenter", "160")
+# db.insert("500w PSU", "Karen Johnson", "Newegg", "80")
+# db.insert("2GB DDR4 Ram", "Karen Johnson", "Newegg", "70")
+# db.insert("24 inch Samsung Monitor", "Sam Smith", "Best Buy", "180")
+# db.insert("NVIDIA RTX 2080", "Albert Kingston", "Newegg", "679")
+# db.insert("600w Corsair PSU", "Karen Johnson", "Newegg", "130")
--- sample_gui/part_manager.py
+++ sample_gui/part_manager.py
@@ -1,24 +1,68 @@
 from tkinter import *
+from tkinter import messagebox
+from db import Database
+
+
+db = Database('store.db')
+selected_item = None
 
 
 def populate_list():
-    print('Populate')
+    parts_list.delete(0, END)
+    for row in db.fetch():
+        parts_list.insert(END, row)
 
 
 def add_item():
-    print('Add')
+    if part_text.get() == '' \
+            or customer_text.get() == '' \
+            or retailer_text.get() == '' \
+            or price_text.get() == '':
+        messagebox.showerror('Required Fields', 'Please include all fields')
+        return
+    db.insert(part_text.get(), customer_text.get(),
+              retailer_text.get(), price_text.get())
+    parts_list.delete(0, END)
+    parts_list.insert(END, (part_text.get(), customer_text.get(),
+                            retailer_text.get(), price_text.get()))
+    clear_text()
+    populate_list()
+
+
+def select_item(event):
+    try:
+        global selected_item
+        index = parts_list.curselection()[0]
+        selected_item = parts_list.get(index)
+        part_entry.delete(0, END)
+        part_entry.insert(END, selected_item[1])
+        customer_entry.delete(0, END)
+        customer_entry.insert(END, selected_item[2])
+        retailer_entry.delete(0, END)
+        retailer_entry.insert(END, selected_item[3])
+        price_entry.delete(0, END)
+        price_entry.insert(END, selected_item[4])
+    except IndexError:
+        pass
 
 
 def remove_item():
-    print('Remove')
+    db.remove(selected_item[0])
+    clear_text()
+    populate_list()
 
 
 def update_item():
-    print('Update')
+    db.update(selected_item[0], part_text.get(), customer_text.get(),
+              retailer_text.get(), price_text.get())
+    populate_list()
 
 
 def clear_text():
-    print('Clear')
+    part_entry.delete(0, END)
+    customer_entry.delete(0, END)
+    retailer_entry.delete(0, END)
+    price_entry.delete(0, END)
 
 
 # Create window object
@@ -26,7 +70,7 @@ app = Tk()
 
 # Part
 part_text = StringVar()
-part_label = Label(app, text='Part Name', font=('bold', 14), pady=20)
+part_label = Label(app, text='Part Name', font=('bold', 14), padx=20)
 part_label.grid(row=0, column=0, sticky=W)
 part_entry = Entry(app, textvariable=part_text)
 part_entry.grid(row=0, column=1)
@@ -40,7 +84,7 @@ customer_entry.grid(row=0, column=3)
 
 # Retailer
 retailer_text = StringVar()
-retailer_label = Label(app, text='Retailer', font=('bold', 14))
+retailer_label = Label(app, text='Retailer', font=('bold', 14), padx=20)
 retailer_label.grid(row=1, column=0, sticky=W)
 retailer_entry = Entry(app, textvariable=retailer_text)
 retailer_entry.grid(row=1, column=1)
@@ -53,16 +97,19 @@ price_entry = Entry(app, textvariable=price_text)
 price_entry.grid(row=1, column=3)
 
 # Parts List (Listbox)
-parts_list = Listbox(app, height=8, width=50)
-parts_list.grid(row=3, column=0, columnspan=3, rowspan=6, pady=20, padx=20)
+parts_list = Listbox(app, height=9, width=60)
+parts_list.grid(row=3, column=0, columnspan=3, rowspan=6, padx=20)
 
 # Create scorllbar
 scrollbar = Scrollbar(app)
-scrollbar.grid(row=3, column=3)
+scrollbar.grid(row=3, column=3, rowspan=6, sticky=N + S + W)
 
 # Set scroll to listbox
 parts_list.configure(yscrollcommand=scrollbar.set)
 scrollbar.configure(command=parts_list.yview)
+
+# Bind part selection
+parts_list.bind('<<ListboxSelect>>', select_item)
 
 # Buttons
 add_btn = Button(app, text='Add Part', width=12, command=add_item)
